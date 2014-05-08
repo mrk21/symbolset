d46931369d39cba180da3a485134309e8f1867e6
Yuichi Murata <mrk21info+github@gmail.com>
2014-05-08 21:49:05 +0900

    Added the symbolset.

--- symbolset/symbolset.hpp
+++ symbolset/symbolset.hpp
@@ -2,7 +2,26 @@
 #define __INCLUDED_SYMBOLSET_SYMBOLSET_HPP__
 
 namespace symbolset {
-    
+    template<class T>
+    class symbolset {
+    public:
+        enum class value_type: uint8_t {};
+        
+    protected:
+        value_type value;
+        
+    public:
+        constexpr symbolset() : value{} {};
+        constexpr symbolset(value_type v) : value(v) {}
+        constexpr explicit symbolset(uint8_t v) : value(static_cast<value_type>(v)) {}
+        
+        constexpr operator value_type() const {
+            return this->value;
+        }
+        constexpr explicit operator int() const {
+            return static_cast<int>(this->value);
+        }
+    };
 }
 
 #endif // __INCLUDED_SYMBOLSET_SYMBOLSET_HPP__
--- test/spec/symbolset_spec.cpp
+++ test/spec/symbolset_spec.cpp
@@ -2,12 +2,58 @@
 #include <symbolset/symbolset.hpp>
 
 namespace symbolset {
+    namespace symbolset_test {
+        struct symtest: public symbolset<symtest> {
+            using symbolset::symbolset;
+            constexpr symtest() : symbolset() {};
+            constexpr symtest(const symbolset & v) : symbolset(static_cast<value_type>(v)) {}
+        };
+        
+        struct symtest_ex: public symtest {
+            using symtest::symtest;
+        };
+        
+        struct symtest_ex2: public symtest_ex {
+            using symtest_ex::symtest_ex;
+        };
+    }
+    
 go_bandit([]{
     using namespace bandit;
     
-    describe("symbolset", [&]{
-        it("should be true", [&]{
-            AssertThat(true, Equals(true));
+    describe("symbolset<T>", [&]{
+        using namespace symbolset_test;
+        
+        describe("#symbolset(int value), #operator int()", [&]{
+            it("should be the value", [&]{
+                symtest v = static_cast<symtest>(3);
+                AssertThat(static_cast<int>(v), Equals(3));
+            });
+        });
+        
+        describe("#symbolset(value_type value)", [&]{
+            it("should set the value", [&]{
+                symtest v = static_cast<symtest::value_type>(3);
+                AssertThat(static_cast<int>(v), Equals(3));
+            });
+        });
+        
+        describe("#operator value_type()", [&]{
+            it("should convert", [&]{
+                symtest v(3);
+                AssertThat(static_cast<symtest::value_type>(v), Equals(static_cast<symtest::value_type>(3)));
+            });
+        });
+        
+        it("should convert the bese class to the derived class", [&]{
+            symtest_ex v1 = static_cast<symtest>(3);
+            AssertThat(static_cast<int>(v1), Equals(3));
+            
+            symtest_ex2 v2 = static_cast<symtest>(3);
+            AssertThat(static_cast<int>(v2), Equals(3));
+            
+            symtest_ex2 v3 = static_cast<symtest_ex>(3);
+            AssertThat(static_cast<int>(v3), Equals(3));
         });
     });
 });
